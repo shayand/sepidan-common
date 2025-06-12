1941e1fbe74568287862302d5fa5b87fec026248
Shayan Davarzani <info@shayand.com>
2025-06-12 16:55:17 +0330

    [ci][task-5] paged response has been added to project

---
+++ src/main/java/net/sepidan/common/dto/PagedResponse.java
@@ -0,0 +1,14 @@
+package net.sepidan.common.dto;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class PagedResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
